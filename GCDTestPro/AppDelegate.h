f2467cb7f390c05efd16e09bfbe26a892ff47394
StaticGa <staticga@gmail.com>
2013-08-30 11:19:38 +0800

    Initial Commit

---
+++ GCDTestPro/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GCDTestPro
+//
+//  Created by Static Ga on 13-8-30.
+//  Copyright (c) 2013年 Static Ga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GCDTestPro/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GCDTestPro
+//
+//  Created by Static Ga on 13-8-30.
+//  Copyright (c) 2013年 Static Ga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
